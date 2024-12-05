d1268c268bfd43c45ec368f42c5711a13fcd6492
ajude <alex.jude@iais.fraunhofer.de>
2024-12-05 14:22:36 +0100

    feat(modalities): registering modalities model

--- lm_eval/models/modalities.py
+++ lm_eval/models/modalities.py
@@ -3,10 +3,11 @@ import torch
 from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, BatchEncoding
 from modalities.models.huggingface_adapters.hf_adapter import HFModelAdapterConfig, HFModelAdapter
 from .huggingface import HFLM
+from lm_eval.api.registry import register_model
 
 TokenSequence = Union[List[int], torch.LongTensor, torch.Tensor, BatchEncoding]
 
-
+@register_model("modalities")
 class Modalities(HFLM):
     def __init__(self, *args, **kwargs):
         AutoConfig.register("modalities", HFModelAdapterConfig)
